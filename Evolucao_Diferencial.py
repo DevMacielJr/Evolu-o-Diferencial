29cd85b015268c99b32b62be9661da8ffc45531f
Edsonjr4 <edsonmaciel661@gmail.com>
2022-08-08 09:10:55 -0300

    .

--- Evolucao_Diferencial.py
+++ Evolucao_Diferencial.py
@@ -102,6 +102,8 @@ def main():
             st = np.array([steps_list])
             #Media e Desvio Padrao do pop_size + best_fitness + steps
             print(f'{pop_size} {dim} {np.mean(bf)} {np.std(bf)} {np.mean(st)} {np.std(st)}')
+            whit open('statistics.dat', 'a') as outfile:
+                print(f'{pop_size} {dim} {np.mean(bf)} {np.std(bf)} {np.mean(st)} {np.std(st)}', file=outfile)
 
 if __name__ == '__main__':
     main()
